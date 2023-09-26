f14be1bbb1761d8c462240a548d5bcbb6c61fc16
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-09-26 18:37:30 -0500

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -2,13 +2,23 @@
 <div class="table-responsive">
   <table class="table">
     <thead>
+      <tr>
       <th>ID</th>
       <th>Name</th>
       <th>Hometown</th>
+      </tr>
     </thead>
     <tbody>
  <?php
-  while ($artist = $artists
+  while ($artist = $artists->fetch_assoc()) {
+   ?>
+    <tr>
+      <td><?php echo $artist['artist_id']; ?></td>
+      <td><?php echo $artist['artist_name']; ?></td>
+      <td><?php echo $artist['hometown']; ?></td>
+    </tr>
+  <?php
+  }
  ?>
     </tbody>
   </table>
