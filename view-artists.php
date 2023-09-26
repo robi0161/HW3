af073a185a87b6e4b9886a66b5b9910e1d1aa408
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-09-26 18:32:14 -0500

    Create view-artists.php

---
+++ view-artists.php
@@ -0,0 +1,15 @@
+<h1>Artists</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Hometown</th>
+    </thead>
+    <tbody>
+ <?php
+  while ($artist = $artists
+ ?>
+    </tbody>
+  </table>
+</div>
