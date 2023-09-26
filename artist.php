5df207228ff93db78c2b651e807e00f6867af608
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-09-26 18:17:24 -0500

    Create artist.php

---
+++ artist.php
@@ -0,0 +1,9 @@
+<?php
+require_once("util-db.php");
+require_once("model-artist.php");
+
+$pageTitle = "Artists";
+include "view-header.php";
+$Artist = selectAtrist()
+include "view-footer.php";
+?>
