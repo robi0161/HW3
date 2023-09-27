6bcab4e717f1917de5aaaaac10d8bf1af8214db2
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-09-27 13:55:03 -0500

    Update model-genres.php

--- model-genres.php
+++ model-genres.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectArtists() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT genre_id, genre_name, genre_description  FROM `artist`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
