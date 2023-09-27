8399db8a4294e48c71337a3e3224a6cffca98729
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-09-27 13:40:04 -0500

    Update genres.php

--- genres.php
+++ genres.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-genres.php");
 
+$pageTitle = "Genres";
+include "view-header.php";
+$genres = selectGenres();
+include "view-genres.php";
+include "view-genres.php";
+?>
