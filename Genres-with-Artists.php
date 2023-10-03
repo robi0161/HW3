8d669e57e409e713116e9149b50a1c5d5fd3a7cb
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-03 17:25:42 -0500

    Update and rename Artists-with-Genres.php to Genres-with-Artists.php

--- Genres-with-Artists.php
+++ Genres-with-Artists.php
@@ -1,9 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-genres.php");
+require_once("model-Genres-with-Artists.php");
 
-$pageTitle = "Genres";
+$pageTitle = "Artists-with-Genres";
 include "view-header.php";
 $Genres = selectGenres();
 include "view-genres.php";
+include "view-footer.php";
 ?>
