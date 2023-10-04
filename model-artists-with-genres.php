3f3e060c9cff3553815b07e28e026573725df0c6
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-03 22:11:20 -0500

    Update model-artists-with-genres.php

--- model-artists-with-genres.php
+++ model-artists-with-genres.php
@@ -12,4 +12,19 @@ function selectArtists() {
         throw $e;
     }
 }
+
+function selectGenresWithArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT g.genre_id, genre_name, genre_description, title, producer, language FROM `genre` g join song s on s.genre_id = g.genre_id where s.artist_id=?");
+       $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
