13a0d312ca63dd008a8eb084a8bf4d304212c839
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-03 21:49:17 -0500

    Update songs-by-genre.php

--- songs-by-genre.php
+++ songs-by-genre.php
@@ -4,7 +4,7 @@ require_once("model-songs-by-genre.php");
 
 $pageTitle = "Songs by Genre";
 include "view-header.php";
-$Songs = selectSongsByGenre($_POST['gid']);
+$songs = selectSongsByGenre($_POST['gid']);
 include "view-songs-by-genre.php";
 include "view-footer.php";
 ?>
