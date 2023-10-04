a5c641deb9ade7321d6bd7c68eeec5631525bc55
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-03 21:33:56 -0500

    Update model-songs-by-genre.php

--- model-songs-by-genre.php
+++ model-songs-by-genre.php
@@ -3,7 +3,7 @@ function selectSongsByGenre($gid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT g.genre_id, genre_name, genre_description, title, producer, language FROM `genre` g join song s on s.genre_id = g.genre_id where s.genre_id=?");
-        $stmt->bind_param("i", $gid);
+    $stmt->bind_param("i", $gid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
