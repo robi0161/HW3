6b340136c196e6b2b4968f1d6d7b3c63bf347f4b
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-23 18:56:54 -0500

    Update view-genres-newform.php

--- view-genres-newform.php
+++ view-genres-newform.php
@@ -15,21 +15,16 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
+        <form method="post" action="">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label for="gName" class="form-label">Genre Name</label>
+    <input type="text" class="form-control" id="gName" name="Genre">
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="gDesc" class="form-label">Genre Description</label>
+    <input type="text" class="form-control" id="gDesc" name="Description">
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Save</button>
 </form>
       </div>
     </div>
