4ae502ba18581c1a53531c849c7f8de30e564a15
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-23 17:34:29 -0500

    Update model-genres.php

--- model-genres.php
+++ model-genres.php
@@ -12,4 +12,47 @@ function selectGenres() {
         throw $e;
     }
 }
+
+function insertGenre($gName, $gDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `genre` (`genre_name`, `genre_description`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $gName, $gDesc);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateGenre($gName, $gDesc, $gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `genre` set `genre_name` = ?, `genre_description` = ? where genre_id = ?");
+        $stmt->bind_param("ssi", $gName, $gDesc, $gid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteGenre($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from genre where genre_id=?");
+        $stmt->bind_param("i", $gid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
