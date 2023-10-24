afef23a6ccbe896f02d93902e18161410c372668
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 13:25:41 -0500

    Update model-artists.php

--- model-artists.php
+++ model-artists.php
@@ -12,4 +12,45 @@ function selectArtists() {
         throw $e;
     }
 }
+function insertArtists($aName, $aHome) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `artist` (`artist_name`, `hometown`) VALUES (?, ?)");
+         $stmt->bind_param("ss", $aName, $aHome);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateArtists($aName, $aHome, $aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `artist` set `artist_name` = ?, `hometown` = ?) where artist_id = ?");
+         $stmt->bind_param("ssi", $aName, $aHome, $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from artist where artist_id=?");
+         $stmt->bind_param("i", $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
