a04031101498b328dd3168555ca092bd03769412
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 16:45:57 -0500

    Update artists-with-genres.php

--- artists-with-genres.php
+++ artists-with-genres.php
@@ -4,6 +4,20 @@ require_once("model-artists-with-genres.php");
 
 $pageTitle = "Artists With Genres";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+      if(insertSong($_POST['aName'], $_POST['gDesc'], $_POST['title'], $_POST['language'], $_POST['producer'])) {
+        echo '<div class="alert alert-success" role="alert">Artists With Genre Added</div>';
+      } else{
+         echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
+
 $Artists = selectArtists();
 include "view-artists-with-genres.php";
 include "view-footer.php";
