fa5a7c0cb392083e86466ef6103d45c181680a32
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 13:38:53 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -4,6 +4,17 @@ require_once("model-artists.php");
 
 $pageTitle = "Artists";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+      if(insertArtists($_POST['aName'], $_POST['aHome'])) {
+        echo '<div class="alert alert-success" role="alert">Artist Added</div>';
+      } else{
+         echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+
 $Artists = selectArtists();
 include "view-artists.php";
 include "view-footer.php";
