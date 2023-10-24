3c1e4f1013ddbf497e7ef7a8d8f3f7553c43be67
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-23 19:01:00 -0500

    Update view-genres-newform.php

--- view-genres-newform.php
+++ view-genres-newform.php
@@ -1,9 +1,9 @@
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newGenreModal">
- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-</svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+   </svg>
 </button>
 
 <!-- Modal -->
@@ -16,16 +16,16 @@
       </div>
       <div class="modal-body">
         <form method="post" action="">
-  <div class="mb-3">
-    <label for="gName" class="form-label">Genre Name</label>
-    <input type="text" class="form-control" id="gName" name="Genre">
-  </div>
-  <div class="mb-3">
-    <label for="gDesc" class="form-label">Genre Description</label>
-    <input type="text" class="form-control" id="gDesc" name="Description">
-  </div>
-  <button type="submit" class="btn btn-primary">Save</button>
-</form>
+           <div class="mb-3">
+             <label for="gName" class="form-label">Genre Name</label>
+             <input type="text" class="form-control" id="gName" name="gName">
+           </div>
+           <div class="mb-3">
+             <label for="gDesc" class="form-label">Genre Description</label>
+             <input type="text" class="form-control" id="gDesc" name="gDesc">
+           </div>
+           <button type="submit" class="btn btn-primary">Save</button>
+          </form>
       </div>
     </div>
   </div>
