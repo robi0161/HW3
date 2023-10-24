4842b744bc56185fab5d20bcb7649fc970914b86
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 01:20:41 -0500

    Update genres.php

--- genres.php
+++ genres.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']){
     case "Add":
-      insertGenre($_POST['gName'], $_POST['gDesc']);
+      if(insertGenre($_POST['gName'], $_POST['gDesc'])) {
+        echo '<div class="alert alert-success" role="alert">Genre Added</div>"';
+      } else{
+         echo '<div class="alert alert-danger" role="alert">Error</div>"';
+      }
       break;
   }
 }
