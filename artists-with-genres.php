2716813598a1ad300117a27aa682d25ea1c93bc2
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 18:01:28 -0500

    Update artists-with-genres.php

--- artists-with-genres.php
+++ artists-with-genres.php
@@ -16,7 +16,7 @@ if (isset($_POST['actionType'])) {
       break;
     
     case "Delete":
-      if(deleteSong($_POST['aid'])) {
+      if(deleteSong($_POST['sid'])) {
         echo '<div class="alert alert-success" role="alert">Song Deleted</div>';
       } else{
          echo '<div class="alert alert-danger" role="alert">Error</div>';
