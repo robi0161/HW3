781c9f6b11153d40f414a04003e6486132647103
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-23 19:13:12 -0500

    Update genres.php

--- genres.php
+++ genres.php
@@ -4,6 +4,15 @@ require_once("model-genres.php");
 
 $pageTitle = "Genres";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+      insertGenre($_POST['gName'], $_POST['gDesc']);
+      break;
+  }
+}
+
 $Genres = selectGenres();
 include "view-genres.php";
 ?>
