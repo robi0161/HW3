d4628a9632c1eeb770bca0c740b1ce60be40a82e
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 15:20:53 -0500

    Update model-artists-with-genres.php

--- model-artists-with-genres.php
+++ model-artists-with-genres.php
@@ -27,4 +27,46 @@ function selectGenresWithArtists($aid) {
         throw $e;
     }
 }
+function insertGenresWithArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT g.genre_id, genre_name, genre_description, title, producer, language FROM `genre` g join song s on s.genre_id = g.genre_id where s.artist_id=?");
+       $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function selectGenresWithArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT g.genre_id, genre_name, genre_description, title, producer, language FROM `genre` g join song s on s.genre_id = g.genre_id where s.artist_id=?");
+       $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function selectGenresWithArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT g.genre_id, genre_name, genre_description, title, producer, language FROM `genre` g join song s on s.genre_id = g.genre_id where s.artist_id=?");
+       $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
