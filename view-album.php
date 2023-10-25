a7d447f9ec30bb87b92bad2059deb86cf43b8c6c
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 20:47:34 -0500

    Update view-album.php

--- view-album.php
+++ view-album.php
@@ -1,1 +1,15 @@
 <h1> Top 5 Albums </h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Release Year</th>
+    </thead>
+    <tbody>
+<?php
+while ($albums = $album
+?>
+    </tbody>
+  </table>
+</div>
