cb14bc5d64c8e801b032153060c8aff7b050f4b8
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 19:36:22 -0500

    Update view-artists-with-genres-editform.php

--- view-artists-with-genres-editform.php
+++ view-artists-with-genres-editform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editArtistWithGenreModal<?php echo $song['song_id']; ?>">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editSongModal<?php echo $song['song_id']; ?>">
    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -7,21 +7,21 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="editArtistWithGenreModal<?php echo $song['song_id']; ?>" tabindex="-1" aria-labelledby="editArtistWithGenreModal<?php echo $song['song_id']; ?>" aria-hidden="true">
+<div class="modal fade" id="editSongModal<?php echo $song['song_id']; ?>" tabindex="-1" aria-labelledby="editSongModal<?php echo $song['song_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editArtistWithGenreModal<?php echo $song['song_id']; ?>">Edit Song</h1>
+        <h1 class="modal-title fs-5" id="editArtistModal<?php echo $artist['artist_id']; ?>">Edit Artist</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
            <div class="mb-3">
-             <label for="aName<?php echo $artist['artist_id']; ?>" class="form-label">Artist</label>
+             <label for="aName<?php echo $artist['artist_id']; ?>" class="form-label">Artist Name</label>
              <input type="text" class="form-control" id="aName<?php echo $artist['artist_id']; ?>" name="aName" value = "<?php echo $artist['artist_name']; ?>">
            </div>
            <div class="mb-3">
-             <label for="aHome<?php echo $genre['genre_id']; ?>" class="form-label">Genre</label>
+             <label for="aHome<?php echo $artist['artist_id']; ?>" class="form-label">Artist Hometown</label>
              <input type="text" class="form-control" id="aHome<?php echo $artist['artist_id']; ?>" name="aHome" value = "<?php echo $artist['hometown']; ?>">
            </div>
            <input type="hidden" name="aid" value="<?php echo $artist['artist_id']; ?>">
