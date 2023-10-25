2710cbea71609eae083b1a5a5c09ce540d1b182b
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 21:56:55 -0500

    Update album.php

--- album.php
+++ album.php
@@ -8,11 +8,12 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']){
     case "Add":
-      if (insertAlbum($_POST['alName'], $_POST['alYear']))
+      if (insertAlbum($_POST['alName'], $_POST['alYear'])){
            echo '<div class="alert alert-success" role="alert">Album Added</div>';
-      } else{
-         echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }else{
+        '<div class="alert alert-danger" role="alert">Error</div>';
       }
+      
       break;
   }
 }
