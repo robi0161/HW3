0eb38162f945fa2821f8465d2dccc1f9ffcab608
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-25 12:18:07 -0500

    Update stream.php

--- stream.php
+++ stream.php
@@ -8,15 +8,15 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']){
     case "Add":
-      if (insertStream($_POST['sName'])){
-           echo '<div class="alert alert-success" role="alert">Streaming Service Added</div>';
+      if (insertStream($_POST['stName'], $_POST['stYear'])){
+           echo '<div class="alert alert-success" role="alert">Stream Added</div>';
       }else{
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }    
       break;
         
     case "Edit":
-      if (updateStream($_POST['sName'], $_POST['stid'])){
+      if (updateStream($_POST['stName'], $_POST['stYear'], $_POST['stid'])){
            echo '<div class="alert alert-success" role="alert">Stream Edited</div>';
       }else{
         echo '<div class="alert alert-danger" role="alert">Error</div>';
