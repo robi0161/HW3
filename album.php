9e6719b0f7884b705463ed5ba2122bcbf1203685
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 22:36:36 -0500

    Update album.php

--- album.php
+++ album.php
@@ -11,7 +11,7 @@ if (isset($_POST['actionType'])){
       if (insertAlbum($_POST['alName'], $_POST['alYear'])){
            echo '<div class="alert alert-success" role="alert">Album Added</div>';
       }else{
-        '<div class="alert alert-danger" role="alert">Error</div>';
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
       }    
       break;
    case "Delete":
