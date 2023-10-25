cf39c6b0929eee0130369f51069a337769fd43db
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-10-24 20:51:44 -0500

    Update view-album.php

--- view-album.php
+++ view-album.php
@@ -2,13 +2,23 @@
 <div class="table-responsive">
   <table class="table">
     <thead>
+      <tr>
       <th>ID</th>
       <th>Name</th>
       <th>Release Year</th>
+      </tr>
     </thead>
     <tbody>
 <?php
-while ($albums = $album
+while ($albums = $album->fetch_assoc()) {
+  ?>
+   <tr>
+     <td><?php echo $albums['album_id']; ?></td>
+     <td><?php echo $albums['album_name']; ?></td>
+     <td><?php echo $albums['release_year']; ?></td>
+   </tr>
+  <?php
+}
 ?>
     </tbody>
   </table>
